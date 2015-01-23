24ffb7ee3d7cded1101c25dc4a529bc7339627ca
Gavin.Ke <asdkda@gmail.com>
2015-01-23 16:12:04 +0800

    Add link for login with facebook

--- template.php
+++ template.php
@@ -6,15 +6,35 @@
  */
 
 
-function survey_theme() {
-    $items = array();
-    
-    $items['surveydate_form'] = array(
-        'render element' => 'form',
-        'template' => 'surveydate',
-        'path' => drupal_get_path('theme', 'survey') . '/templates',
-    );
+/**
+ * Implements hook_theme().
+ */
+function survey_theme(&$existing, $type, $theme, $path) {
+  $hook_theme = array(
+    'surveydate_form' => array(
+      'render element' => 'form',
+      'template' => 'surveydate',
+      'path' => drupal_get_path('theme', 'survey') . '/templates',
+    ),
+    'user_login_block' => array(
+      'render element' => 'form',
+    ),
+  );
 
-    return $items;
+  return $hook_theme;
+}
 
+function survey_user_login_block($variables) {
+  $form = $variables['form'];
+
+  /* Copy from user.module */
+  $items = array();
+  if (variable_get('user_register', USER_REGISTER_VISITORS_ADMINISTRATIVE_APPROVAL)) {
+    $items[] = l(t('Create new account'), 'user/register', array('attributes' => array('title' => t('Create a new user account.'))));
+  }
+  $items[] = l(t('Request new password'), 'user/password', array('attributes' => array('title' => t('Request new password via e-mail.'))));
+  $items[] = l(t('Login with Facebook'), 'user/simple-fb-connect', array('attributes' => array('title' => t('Login with Facebook.'))));
+  $form['links'] = array('#markup' => theme('item_list', array('items' => $items)));
+
+  return drupal_render_children($form);
 }
