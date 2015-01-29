50b48386f6519620c68d721f2c18b91fa8fc417a
krammer0 <krammer0@gmail.com>
2015-01-29 23:36:01 +0800

    add domain

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -4,8 +4,9 @@ print render($form['name']);
 print render($form['pass']);
 print render($form['actions']);
 global $base_url;
+global $domain;
 ?>
 
-	<a href="<?php print $base_url;?>/user/register" type="button" class="btn btn-success" >Create New Account</a>
-	<a href="<?php print $base_url;?>/user/password" type="button" class="btn btn-warning" >Request New Password</a>
-	<a href="<?php print $base_url;?>/user/simple-fb-connect" type="button" class="btn btn-primary" >Login with Facebook</a>
+	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/register" type="button" class="btn btn-success" >Create New Account</a>
+	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/password" type="button" class="btn btn-warning" >Request New Password</a>
+	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/simple-fb-connect" type="button" class="btn btn-primary" >Login with Facebook</a>
