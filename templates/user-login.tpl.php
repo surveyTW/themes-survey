8859c82d5fc7078ffa91dce0c552fe696f570acd
krammer0 <krammer0@gmail.com>
2015-01-29 23:29:10 +0800

    use base_url

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -3,9 +3,9 @@
 print render($form['name']);
 print render($form['pass']);
 print render($form['actions']);
-
+global $base_url;
 ?>
 
-	<a href="../user/register" type="button" class="btn btn-success" >Create New Account</a>
-	<a href="../user/password" type="button" class="btn btn-warning" >Request New Password</a>
-	<a href="../user/simple-fb-connect" type="button" class="btn btn-primary" >Login with Facebook</a>
+	<a href="<?php print $base_url;?>/user/register" type="button" class="btn btn-success" >Create New Account</a>
+	<a href="<?php print $base_url;?>/user/password" type="button" class="btn btn-warning" >Request New Password</a>
+	<a href="<?php print $base_url;?>/user/simple-fb-connect" type="button" class="btn btn-primary" >Login with Facebook</a>
