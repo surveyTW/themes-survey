84ee9e3915f7507807269617372970653e0c18f1
krammer0 <krammer0@gmail.com>
2015-01-30 18:55:19 +0800

    render all or drupal can't read

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -2,7 +2,7 @@
 
 print render($form['name']);
 print render($form['pass']);
-print render($form['actions']);
+print render($form['actions']['submit']);
 global $base_url;
 global $domain;
 ?>
@@ -10,3 +10,7 @@ global $domain;
 	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/register" type="button" class="btn btn-success" >Create New Account</a>
 	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/password" type="button" class="btn btn-warning" >Request New Password</a>
 	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/simple-fb-connect" type="button" class="btn btn-primary" >Login with Facebook</a>
+
+<div class="hidden">
+  <?php print drupal_render_children($form); ?>
+</div>
