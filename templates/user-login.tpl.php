3df98514245d3d50b2209ba0afd440a2415eea97
krammer0 <krammer0@gmail.com>
2015-02-05 00:33:12 +0800

    tune user login

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -1,16 +1,24 @@
+<?php
+drupal_add_css(drupal_get_path('theme', 'survey') . '/css/font-awesome.min.css', array('group' => CSS_THEME, 'type' => 'file'));
+?>
+<div class="col-xs-12 col-md-6 col-md-offset-3">
+<div class="container-fluid">
 <?php 
-
-print render($form['name']);
-print render($form['pass']);
-print render($form['actions']['submit']);
 global $base_url;
 global $domain;
+print render($form['name']);
+print render($form['pass']);
 ?>
-
-	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/register" type="button" class="btn btn-success" >Create New Account</a>
-	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/password" type="button" class="btn btn-warning" >Request New Password</a>
-	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/simple-fb-connect" type="button" class="btn btn-primary" >Login with Facebook</a>
-
+<div class="form-group">
+	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/password">忘記密碼?</br></a>
+</div>
+<?php
+print render($form['actions']['submit']);
+?>
+	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/simple-fb-connect" type="button" class="btn btn-primary" ><i class="fa fa-facebook-official"></i> FB一鍵登入</a>
+	<a href="<?php print $base_url;?>/<?php print $domain;?>/user/register" type="button" class="btn btn-success" >註冊</a>
+</div>
+</div>
 <div class="hidden">
   <?php print drupal_render_children($form); ?>
 </div>
