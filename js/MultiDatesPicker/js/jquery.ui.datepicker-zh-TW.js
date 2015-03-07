35467b34beee9bae3bc103279d2392b1e4e7bab5
JohnnyMayson <johnnymayson101@gmail.com>
2015-03-07 19:59:25 +0800

    surveydate.tpl.php

--- js/MultiDatesPicker/js/jquery.ui.datepicker-zh-TW.js
+++ js/MultiDatesPicker/js/jquery.ui.datepicker-zh-TW.js
@@ -12,7 +12,7 @@
                 dayNames: ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
                 dayNamesShort: ['周日','周一','周二','周三','周四','周五','周六'],
                 dayNamesMin: ['日','一','二','三','四','五','六'],
-                dateFormat: 'yyyy/mm/dd',
+                dateFormat: 'mm/dd/yyyy',
                 firstDay: 1,
                 prevText: '&#x3c;上月', prevStatus: '',
                 prevJumpText: '&#x3c;&#x3c;', prevJumpStatus: '',
