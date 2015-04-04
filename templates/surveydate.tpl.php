5f41f9a9e0cead52f77a0109d6f4919e75c44535
JohnnyMayson <johnnymayson101@gmail.com>
2015-04-04 20:49:39 +0800

    surveydate.tpl.php

--- templates/surveydate.tpl.php
+++ templates/surveydate.tpl.php
@@ -25,120 +25,96 @@ if ($form['name']['#value'] == null) {
 };
 ?>
 
-<div id="survey-body">
-  <div id="first-step" class="show">
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-        <div class="btn-group pull-right">
-          <a href="#" type="button" class="btn btn-success" id="1st-Next">Next</a>
+<div class="container">
+  <div id="survey-body">
+    <div id="first-step" class="show">
+      <h1 class="text-left">
+        <img src="sites/all/themes/survey/img/meet_step_title.png" alt="建立聚會">
+      </h1>
+      <form>
+        <div class="col-md-4 form-group">
+          <label for="meet-title">
+            <span class="glyphicon glyphicon-bookmark"></span>聚會名稱
+          </label>
+          <?php print render($form['title']); ?>
         </div>
-        <h4>1.建立聚會基本資料</h4>
-      </div>
-      <div class="panel-body">
-        <form>
-          <div class="col-md-12 form-group">
-            <div class="input-group">
-              <div class="input-group-addon"><span class="glyphicon glyphicon-bookmark"></span></div>
-              <?php print render($form['title']); ?>
+        <div class="col-md-6 form-group">
+          <label for="meet-detail">
+            <span class="glyphicon glyphicon-list"></span>備註
+          </label>
+          <?php print render($form['description']); ?>
+        </div>
+        <div class="col-md-4 form-group">
+          <label for="meet-chairman">
+            <span class="glyphicon glyphicon-user"></span>主辦者
+          </label>
+          <?php print render($form['name']); ?>
+        </div>
+        <div class="col-md-6 form-group">
+          <label for="meet-email">
+            <span class="glyphicon glyphicon-envelope"></span>聯絡信箱
+          </label>
+          <?php print render($form['email']); ?>
+        </div>
+        <div class="col-md-4 form-group">
+          <label for="meet-calendar">
+            <span class="glyphicon glyphicon-calendar"></span>日期<span>[可複選]</span>
+          </label>
+          <div id="selectdate"></div>
+        </div>
+        <div class="col-md-6 form-group">
+          <label for="meet-time">
+            <span class="glyphicon glyphicon-time"></span>時間
+          </label>
+          <div id="selecttime"></div>
+        </div>
+        <div class="col-md-12 btn-group ">
+          <a href="#" type="button" id="first-Next" class="btn pull-right"></a>
+        </div>
+      </form>
+    </div>
+    <div id="second-step" class="hidden">
+      <div class="row">
+        <div class="col-md-9">
+          <form class="form-horizontal">
+            <div class="col-md-12 form-group">
+              <label for="meet-location" class="col-md-2">
+                <span class="glyphicon glyphicon-map-marker"></span>地點
+              </label>
+              <?php print render($form['location']); ?>
+              <div id="surveymap" class="col-md-12 form-group"></div>
             </div>
-          </div>
-          <div class="col-md-6 form-group">
-            <div class="input-group">
-              <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span></div>
-              <?php print render($form['name']); ?>
+            <div class="col-md-12 btn-group ">
+              <?php print render($form['submit']); ?>
+              <a href="#" type="button" id="second-Prev" class="btn pull-right"></a>
             </div>
-          </div>
-          <div class="col-md-6 form-group">
-            <div class="input-group">
-              <div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></div>
-              <?php print render($form['email']); ?>
-            </div>
-          </div>
-          <div class="col-md-6 form-group">
-            <label>聚會細節</label>
-            <?php print render($form['description']); ?>
-          </div>
-          <div class="col-md-6 form-group">
-            <label for="exampleInputName2">聚會日期</label>
-            <div id="selectdate" class=""></div>
-          </div>
-        </form>
+          </form>
+        </div>
+        <div class="col-md-3">
+          <script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <!-- 160x600, 已建立 2010/9/30 -->
+          <ins class="adsbygoogle"
+               style="display:inline-block;width:160px;height:600px"
+               data-ad-client="ca-pub-4355428491296720"
+               data-ad-slot="5821892280"></ins>
+          <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+        </div>
       </div>
     </div>
   </div>
-  <div id="second-step" class="hidden">
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-        <div class="btn-group pull-right">
-          <a href="#" type="button" id="2nd-Prev" class="btn btn-success">Prev</a>
-          <a href="#" type="button" id="2nd-Next" class="btn btn-success">Next</a>
-        </div>
-        <h4>2.聚會時間</h4>
-      </div>
-      <div class="panel-body">
-        <div class="form-group">
-          <button type="button" class="btn btn-warning" id="add-timeslots">
-            <span class="glyphicon glyphicon-plus"></span>新增時間</button>
-          <button type="button" class="btn btn-warning" id="copy-from-first-row">
-            <span class="glyphicon glyphicon-file"></span>複製時間</button>
-        </div>
-        <div class="row form-group">
-          <div id="selecttime" class="col-md-12"></div>
-        </div>
-      </div>
+  <div id="survey-end">
+    <div class="jumbotron text-center">
+      <h2>問卷製作完成!</h2>
+      <p>
+        分享問卷網址 <a id="text-to-copy" href=""
+          <?php print $form['survey_path']['#value'] ?>" data-toggle="tooltip" data-placement="bottom" title="請複製網址"><?php print $form['survey_path']['#value'] ?>
+        </a>
+      </p>
     </div>
-  </div>
-  <div id="third-step" class="hidden">
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-        <div class="btn-group pull-right">
-          <a href="#" id="3rd-Prev" type="button" class="btn btn-success">Prev</a>
-          <?php print render($form['submit']); ?>
-        </div>
-        <h4>3.聚會地點</h4>
-      </div>
-      <div class="panel-body">
-        <div class="col-md-12 form-group">
-          <div class="input-group">
-            <div class="input-group-addon"><span class="glyphicon glyphicon-map-marker"></span></div>
-            <?php print render($form['location']); ?>
-          </div>
-          <div id="surveymap" class="col-md-12 form-group"></div>
-        </div>
-      </div>
-    </div>
-
   </div>
 </div>
-<div id="survey-end">
-  <div class="jumbotron">
-    <div class="alert alert-danger" role="alert">恭喜你 你已完成問卷!</div>
-    <form class="form-horizontal">
-      <div class="form-group">
-        <label class="col-xs-2 control-label">問卷網址</label>
-        <div class="col-xs-8">
-          <label><a id="text-to-copy" href="<?php print $form['survey_path']['#value'] ?>" data-toggle="tooltip" data-placement="bottom" title="請複製網址"><?php print $form['survey_path']['#value'] ?></a></label>
-        </div>
-      </div>
-    </form>
-  </div>
-  <div class="line_button"><a href="http://line.naver.jp/R/msg/text/?<?php print $form['survey_path']['#value']; ?>">
-      <img src="http://img1.cna.com.tw/www/images/linebutton_32x32.png" width="32" height="32" alt="LINE分享給好友" title="LINE分享給好友"></a>
-  </div>
-</div>
-
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id))
-      return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=<?php print variable_get('simple_fb_connect_appid', 0); ?>&version=v2.0";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
-<div class="fb-share-button" data-href="<?php print $form['survey_path']['#value']; ?>" data-layout="button"></div>
 
 <?php print render($form['date']); ?>
 
