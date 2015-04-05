c418d406401fba01c883a56f4c8f4a4f76af2ced
JohnnyMayson <johnnymayson101@gmail.com>
2015-04-05 15:01:04 +0800

    page--front.tpl.php

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -81,9 +81,9 @@ global $domain;
   <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="
+      <a class="logo navbar-btn pull-left" href=""
         <?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src=""<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
 
