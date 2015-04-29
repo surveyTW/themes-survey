e941a7ba503aae740bf26d0112dd70b53278399e
JohnnyMayson <johnnymayson101@gmail.com>
2015-04-29 20:29:33 +0800

    user-login.tpl.php

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 drupal_add_css(drupal_get_path('theme', 'survey') . '/css/font-awesome.min.css', array('group' => CSS_THEME, 'type' => 'file'));
 ?>
 <div class="col-xs-12 col-md-6 col-md-offset-3">
@@ -10,13 +10,13 @@ drupal_add_css(drupal_get_path('theme', 'survey') . '/css/font-awesome.min.css',
     print render($form['pass']);
     ?>
     <div class="form-group">
-      <a href="<?php print $base_url; ?>/<?php print $domain; ?>/user/password">忘記密碼?</br></a>
+      <a href="/user/password">忘記密碼?</br></a>
     </div>
     <?php
     print render($form['actions']['submit']);
     ?>
-    <a href="<?php print $base_url; ?>/<?php print $domain; ?>/user/simple-fb-connect" type="button" class="btn btn-primary" ><i class="fa fa-facebook-official"></i> FB一鍵登入</a>
-    <a href="<?php print $base_url; ?>/<?php print $domain; ?>/user/register" type="button" class="btn btn-success" >註冊</a>
+    <a href="/user/simple-fb-connect" type="button" class="btn btn-primary" ><i class="fa fa-facebook-official"></i> FB一鍵登入</a>
+    <a href="/user/register" type="button" class="btn btn-success" >註冊</a>
   </div>
 </div>
 <div class="hidden">
